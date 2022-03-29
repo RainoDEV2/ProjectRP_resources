0fa1abacb5bd6d23c03a09e355bd245915e82b57
Tronix <AxelTronix@users.noreply.github.com>
2022-03-29 01:36:17 +0100

    forgot something oops

---
+++ resources/[prp]/prp-storages/shared/sh_config.lua
@@ -0,0 +1,13 @@
+Config = Config or {}
+
+Config.StorageContainers = {}
+
+-- Editable
+
+Config.CoreExport = exports['prp-core']:GetCoreObject() 
+
+Config.EstateJob = "realestate"
+
+Config.StashSlots = 15 
+Config.MaxStashWeight = 1000000 
+
