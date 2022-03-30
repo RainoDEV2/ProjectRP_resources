c671ac49a1689ce91135a3a53ac3d65fc46d2d65
Henry <henrynorth@outlook.com>
2022-03-30 19:39:52 +0100

    Add idle cam

--- resources/[prp]/prp-core/server/commands.lua
+++ resources/[prp]/prp-core/server/commands.lua
@@ -228,3 +228,47 @@ ProjectRP.Commands.Add('me', 'Show local message', {name = 'message', help = 'Me
         end
     end
 end, 'user')
+
+-- Idle cam toggle commands
+
+ProjectRP.Commands.Add('idlecamoff', 'Disables the idle cam', {}, false, function(source)
+    local src = source
+    DisableIdleCamera(true)
+    SetResourceKvp("idleCam", "off")
+    TriggerClientEvent('ProjectRP:Notify', src, 'Idle cam is now off')
+end, 'user')
+
+ProjectRP.Commands.Add('idlecamon', 'Re-enables the idle cam', {}, false, function(source)
+    local src = source
+    DisableIdleCamera(false)
+    SetResourceKvp("idleCam", "on")
+    TriggerClientEvent('ProjectRP:Notify', src, 'Idle cam is now on')
+end, 'user')
+
+-- RegisterCommand('idlecamoff', function() -- help2 31, 167, 9
+--     TriggerEvent('chat:addMessage', {
+--         color = {227,8,0},
+--         multiline = true,
+--         args = {'[COMMANDS]', 'Idle Cam Is Now Off'}
+--     })
+--     DisableIdleCamera(true)
+--     SetResourceKvp("idleCam", "off")
+-- end)
+
+-- RegisterCommand('idlecamon', function() -- help2 31, 167, 9
+--     TriggerEvent('chat:addMessage', {
+--         color = {31,167,9},
+--         multiline = true,
+--         args = {'[COMMANDS]', 'Idle Cam Is Now On'}
+--     })
+--     DisableIdleCamera(false)
+--     SetResourceKvp("idleCam", "on")
+-- end)
+
+-- Citizen.CreateThread(function()
+--     TriggerEvent("chat:addSuggestion", "/idlecamon", "Re-enables the idle cam")
+--     TriggerEvent("chat:addSuggestion", "/idlecamoff", "Disables the idle cam")
+    
+--     local idleCamDisabled = GetResourceKvpString("idleCam") ~= "on"
+--     DisableIdleCamera(idleCamDisabled)
+-- end)
