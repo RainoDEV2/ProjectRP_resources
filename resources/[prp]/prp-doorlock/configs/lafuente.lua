8a2ed9e99d844552f932486a0f587f7ea8c96940
ProjectRP100 <projectrp100@gmail.com>
2022-04-05 16:50:13 -0700

    Create lafuente.lua

---
+++ resources/[prp]/prp-doorlock/configs/lafuente.lua
@@ -0,0 +1,145 @@
+
+
+-- Front Door created by Parker
+Config.DoorList['Front Door'] = {
+    doorType = 'double',
+    authorizedCitizenIDs = { ['PFS83286'] = true },
+    doors = {
+        {objName = 1504256620, objYaw = 270.00003051758, objCoords = vec3(1395.920288, 1142.904053, 114.790207)},
+        {objName = 262671971, objYaw = 89.999961853027, objCoords = vec3(1395.919922, 1140.704590, 114.790207)}
+    },
+    audioRemote = false,
+    doorRate = 1.0,
+    distance = 2,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- frontdoor2 created by Parker
+Config.DoorList['frontdoor2'] = {
+    doorType = 'double',
+    authorizedCitizenIDs = { ['PFS83286'] = true },
+    doors = {
+        {objName = -52575179, objYaw = 89.999961853027, objCoords = vec3(1390.665771, 1133.317261, 114.480843)},
+        {objName = -1032171637, objYaw = 89.999961853027, objCoords = vec3(1390.665771, 1131.117065, 114.480843)}
+    },
+    audioRemote = false,
+    doorRate = 1.0,
+    distance = 2,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- sidedoor1 created by Parker
+Config.DoorList['sidedoor1'] = {
+    doorType = 'double',
+    authorizedCitizenIDs = { ['PFS83286'] = true },
+    doors = {
+        {objName = -52575179, objYaw = 179.99998474121, objCoords = vec3(1399.392944, 1128.314209, 114.483597)},
+        {objName = -1032171637, objYaw = 179.99998474121, objCoords = vec3(1401.590454, 1128.314209, 114.483597)}
+    },
+    audioRemote = false,
+    doorRate = 1.0,
+    distance = 2,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- basement created by Parker
+Config.DoorList['basement'] = {
+    distance = 2,
+    fixText = false,
+    doorType = 'door',
+    audioRemote = false,
+    authorizedCitizenIDs = { ['PFS83286'] = true },
+    objYaw = 180.06739807129,
+    objCoords = vec3(1407.546509, 1128.328735, 114.485451),
+    doorRate = 1.0,
+    objName = 262671971,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- backdoor created by Parker
+Config.DoorList['backdoor'] = {
+    doorType = 'double',
+    authorizedCitizenIDs = { ['PFS83286'] = true },
+    doors = {
+        {objName = -1032171637, objYaw = 89.667892456055, objCoords = vec3(1408.157471, 1158.955811, 114.487251)},
+        {objName = -52575179, objYaw = 90.11840057373, objCoords = vec3(1408.167480, 1161.154785, 114.487251)}
+    },
+    audioRemote = false,
+    doorRate = 1.0,
+    distance = 2,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- backdoor2 created by Parker
+Config.DoorList['backdoor2'] = {
+    doorType = 'double',
+    authorizedCitizenIDs = { ['PFS83286'] = true },
+    doors = {
+        {objName = -1032171637, objYaw = 90.312919616699, objCoords = vec3(1408.170532, 1163.632813, 114.487251)},
+        {objName = -52575179, objYaw = 90.023124694824, objCoords = vec3(1408.165527, 1165.833740, 114.487251)}
+    },
+    audioRemote = false,
+    doorRate = 1.0,
+    distance = 2,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- frontdoor3 created by Parker
+Config.DoorList['frontdoor3'] = {
+    doorType = 'double',
+    authorizedCitizenIDs = { ['PFS83286'] = true },
+    doors = {
+        {objName = -52575179, objYaw = 89.667892456055, objCoords = vec3(1390.424438, 1163.437744, 114.487251)},
+        {objName = -1032171637, objYaw = 89.667892456055, objCoords = vec3(1390.411499, 1161.240723, 114.487251)}
+    },
+    audioRemote = false,
+    doorRate = 1.0,
+    distance = 2,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- tableroom created by Parker
+Config.DoorList['tableroom'] = {
+    distance = 2,
+    fixText = false,
+    doorType = 'door',
+    audioRemote = false,
+    authorizedCitizenIDs = { ['PFS83286'] = true },
+    objYaw = 89.999961853027,
+    objCoords = vec3(1400.996216, 1158.950562, 114.483452),
+    doorRate = 1.0,
+    objName = 736699661,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- office created by Parker
+Config.DoorList['office'] = {
+    distance = 2,
+    fixText = false,
+    doorType = 'door',
+    audioRemote = false,
+    authorizedCitizenIDs = { ['PFS83286'] = true },
+    objYaw = 270.00003051758,
+    objCoords = vec3(1397.062378, 1164.739746, 114.483452),
+    doorRate = 1.0,
+    objName = 736699661,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
