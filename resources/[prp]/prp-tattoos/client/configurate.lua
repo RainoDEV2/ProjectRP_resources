c2731df63506e81f64498ff636d4a03c938e873f
Parker <aidanparker321@gmail.com>
2022-04-09 16:22:41 +0100

    Tattoo fixes

--- resources/[prp]/prp-tattoos/client/configurate.lua
+++ resources/[prp]/prp-tattoos/client/configurate.lua
@@ -19,28 +19,10 @@ tattoos_list = {
     {title = "Hair Degrade", dlc = "hair_degrade", price = 5, qty = 65, acquired = 0, has = false, current = 0}, 
     
     -- Normal
-    {title = "Beach", dlc = "mpbeach_overlays", price = 100, qty = 31, acquired = 0, has = false, current = 0}, 
-    {title = "Business", dlc = "mpbusiness_overlays", price = 100, qty = 26, acquired = 0, has = false, current = 0}, 
-    {title = "Airraces", dlc = "mpairraces_overlays", price = 100, qty = 16, acquired = 0, has = false, current = 0}, 
-    {title = "Biker", dlc = "mpbiker_overlays", price = 100, qty = 122, acquired = 0, has = false, current = 0}, 
-    {title = "Christmas2", dlc = "mpchristmas2_overlays", price = 100, qty = 60, acquired = 0, has = false, current = 0}, 
-    {title = "Gunrunning", dlc = "mpgunrunning_overlays", price = 100, qty = 62, acquired = 0, has = false, current = 0}, 
-    {title = "Hipster", dlc = "mphipster_overlays", price = 100, qty = 98, acquired = 0, has = false, current = 0}, 
-    {title = "Importexport", dlc = "mpimportexport_overlays", price = 100, qty = 24, acquired = 0, has = false, current = 0}, 
-    {title = "Lowrider", dlc = "mplowrider_overlays", price = 100, qty = 34, acquired = 0, has = false, current = 0}, 
-    {title = "Lowrider2", dlc = "mplowrider2_overlays", price = 100, qty = 32, acquired = 0, has = false, current = 0}, 
-    {title = "Luxe", dlc = "mpluxe_overlays", price = 100, qty = 30, acquired = 0, has = false, current = 0}, 
-    {title = "Luxe2", dlc = "mpluxe2_overlays", price = 100, qty = 34, acquired = 0, has = false, current = 0}, 
-    {title = "Smuggler", dlc = "mpsmuggler_overlays", price = 100, qty = 52, acquired = 0, has = false, current = 0}, 
-    {title = "Stunt", dlc = "mpstunt_overlays", price = 100, qty = 100, acquired = 0, has = false, current = 0}, 
-    {title = "Multiplayer", dlc = "multiplayer_overlays", price = 100, qty = 132, acquired = 0, has = false, current = 0}, 
-    {title = "Heists", dlc = "mpheist3_overlays", price = 100, qty = 90, acquired = 0, has = false, current = 0}, 
-    {title = "Heists2", dlc = "mpheist4_overlays", price = 100, qty = 66, acquired = 0, has = false, current = 0}, 
-    {title = "Vinewood", dlc = "mpvinewood_overlays", price = 100, qty = 66, acquired = 0, has = false, current = 0}, 
-
-    -- New
-    {title = "Christmas2017", dlc = "mpchristmas2017_overlays", price = 1, qty = 60, acquired = 0, has = false, current = 0}, 
-    {title = "Christmas2018", dlc = "mpchristmas2018_overlays", price = 100, qty = 2, acquired = 0, has = false, current = 0}, 
+    {title = "Head", dlc = "mpbeach_overlays", price = 100, qty = 99, acquired = 0, has = false, current = 0}, 
+    {title = "Torso", dlc = "mpbusiness_overlays", price = 100, qty = 516, acquired = 0, has = false, current = 0}, 
+    {title = "Arms", dlc = "mpairraces_overlays", price = 100, qty = 351, acquired = 0, has = false, current = 0}, 
+    {title = "Legs", dlc = "mpbiker_overlays", price = 100, qty = 160, acquired = 0, has = false, current = 0}, 
 }
 
 -- Extra settings
