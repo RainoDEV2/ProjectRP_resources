e7e66192ab08e7fc84168fd3bba75f0dbe0684a2
ProjectRP100 <projectrp100@gmail.com>
2022-04-10 13:42:02 -0700

    Gang door locks

---
+++ resources/[prp]/prp-doorlock/configs/families.lua
@@ -0,0 +1,81 @@
+
+
+-- door1 created by Parker
+Config.DoorList['door1'] = {
+    objCoords = vec3(-148.567200, -1596.742798, 35.178482),
+    doorRate = 1.0,
+    fixText = false,
+    distance = 2,
+    doorType = 'door',
+    objName = 852775515,
+    audioRemote = false,
+    authorizedGangs = { ['families'] = 0 },
+    objYaw = 230.00016784668,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- door2 created by Parker
+Config.DoorList['door2'] = {
+    objCoords = vec3(-150.592072, -1596.214478, 35.121975),
+    doorRate = 1.0,
+    fixText = false,
+    distance = 2,
+    doorType = 'door',
+    objName = 1770281453,
+    audioRemote = false,
+    authorizedGangs = { ['families'] = 0 },
+    objYaw = 339.99990844727,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- door3 created by Parker
+Config.DoorList['door3'] = {
+    objCoords = vec3(-157.727020, -1596.476440, 35.257214),
+    doorRate = 1.0,
+    fixText = false,
+    distance = 2,
+    doorType = 'door',
+    objName = 1150875108,
+    audioRemote = false,
+    authorizedGangs = { ['families'] = 0 },
+    objYaw = 231.24998474121,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- door4 created by Parker
+Config.DoorList['door4'] = {
+    objCoords = vec3(-139.911545, -1600.188110, 35.178482),
+    doorRate = 1.0,
+    fixText = false,
+    distance = 2,
+    doorType = 'door',
+    objName = 852775515,
+    audioRemote = false,
+    authorizedGangs = { ['families'] = 0 },
+    objYaw = 340.00003051758,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- door5 created by Parker
+Config.DoorList['door5'] = {
+    objCoords = vec3(-136.150009, -1602.673096, 35.180233),
+    doorRate = 1.0,
+    fixText = false,
+    distance = 2,
+    doorType = 'door',
+    objName = 486670049,
+    audioRemote = false,
+    authorizedGangs = { ['families'] = 0 },
+    objYaw = 250.55706787109,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
