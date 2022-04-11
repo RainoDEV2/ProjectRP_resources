9d95510f64412dfcab802dce7891d03eb40dc6e4
Tronix <AxelTronix@users.noreply.github.com>
2022-04-11 18:53:50 +0100

    Cleaning Tables n Showers

--- resources/[prp]/prp-prison/client/jobs.lua
+++ resources/[prp]/prp-prison/client/jobs.lua
@@ -128,3 +128,196 @@ CreateThread(function()
         end
     end
 end)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--  
+--  S H O W E R C L E A N E R 
+-- 
+
+
+
+
+function drawTxt(x,y ,width,height,scale, text, r,g,b,a, outline)
+    SetTextFont(0)
+    SetTextProportional(0)
+    SetTextScale(scale, scale)
+    SetTextColour(r, g, b, a)
+    SetTextDropShadow(0, 0, 0, 0,255)
+    SetTextEdge(1, 0, 0, 0, 255)
+    SetTextDropShadow()
+    if(outline)then
+        SetTextOutline()
+    end
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(x - width/2, y - height/2 + 0.005)
+end
+
+local IsCleaning = false
+
+local IsEating = false
+
+--local cleanx = vector3(1730.9069824219, 2501.6616210938, 45.819358825684)
+
+local food = vector3(1781.1302490234, 2557.5888671875, 45.6729431152)
+
+local IsCleaningTable = false
+
+local SwappingCharacters = false
+
+-- local TimeR = TimeRemaining
+-- local minutes = TimeR
+
+local function DrawText3D(coords, text)
+    local onScreen,_x,_y=World3dToScreen2d(coords.x,coords.y,coords.z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+
+    SetTextScale(0.40, 0.40)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextDropShadow()
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x,_y)
+end
+
+function JobDoneCleaning(shit,shit2)
+        ProjectRP.Functions.Notify("You've worked some time off your sentence.")
+        jailTime = jailTime - math.random(shit, shit2)
+
+end
+
+Citizen.CreateThread(function()
+    Citizen.Wait(3000)
+    while true do
+        if inJail then
+            Citizen.Wait(1)
+--             local cleanx = vector3(1730.9069824219, 2501.6616210938, 45.819358825684)
+--             if (GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1), 0), cleanx.x,cleanx.y,cleanx.z, true) < 4.0) and not IsCleaning then
+--                 if IsControlJustPressed(1, 38) and not SwappingCharacters then
+--                     IsCleaning = true
+
+
+--                     ProjectRP.Functions.Progressbar("clean_Shower", "Cleaning Showers", 45000, false, true, {
+--                         disableMovement = true,
+--                         disableCarMovement = true,
+--                         disableMouse = false,
+--                         disableCombat = true,
+--                     }, {
+--                         animDict = "amb@world_human_janitor@male@base",
+--                         anim = "base",
+--                         flags = 16,
+--                     }, {}, {}, function() -- Done
+--                         IsCleaning = false
+--                         StopAnimTask(PlayerPedId(), "amb@world_human_janitor@male@base", "base", 1.0)
+--                         JobDoneCleaning(1,4)
+--                     end, function() -- Cancel
+--                         IsCleaning = false
+--                         StopAnimTask(PlayerPedId(), "amb@world_human_janitor@male@base", "base", 1.0)
+--                         ProjectRP.Functions.Notify("Process Canceled..", "error")
+--                     end)
+
+--                 end
+
+--                 DrawText3D({
+--                     x = cleanx.x,
+--                     y = cleanx.y,
+--                     z = cleanx.z + 0.2
+--                 }, "Press [~g~E~w~] to start cleaning the showers")
+--             end
+-- -- table
+           local cleanT = vector3(1759.2132568359, 2485.8781738281, 45.817295074463)
+            if (GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1), 0), cleanT.x,cleanT.y,cleanT.z, true) < 4.0) and not IsCleaningTable then
+                if IsControlJustPressed(1, 38) then
+                    IsCleaningTable = true
+-
+                    ProjectRP.Functions.Progressbar("clean_Table", "Cleaning Table", 20000, false, true, {
+                        disableMovement = true,
+                        disableCarMovement = true,
+                        disableMouse = false,
+                        disableCombat = true,
+                    }, {
+                        animDict = "mini@repair",
+                        anim = "fixing_a_ped",
+                        flags = 16,
+                    }, {}, {}, function() -- Done
+                        IsCleaningTable = false
+                        StopAnimTask(PlayerPedId(), "mini@repair", "fixing_a_ped", 1.0)
+                        JobDoneCleaning(1,4)
+                    end, function() -- Cancel
+                        IsCleaningTable = false
+                        StopAnimTask(PlayerPedId(), "mini@repair", "fixing_a_ped", 1.0)
+                        ProjectRP.Functions.Notify("Process Canceled..", "error")
+                    end)
+
+                end
+
+                DrawText3D({
+                    x = cleanT.x,
+                    y = cleanT.y,
+                    z = cleanT.z + 0.2
+                }, "Press [~g~E~w~] to start cleaning Tables")
+            end
+
+
+
+        else
+            Citizen.Wait(5000)
+        end
+    end
+end)
+
+
+
+RegisterNetEvent("Clean:Showers")
+AddEventHandler("Clean:Showers", function()
+
+    if inJail then
+        if not IsCleaning then
+            IsCleaning = true
+            local playerPed = GetPlayerPed(-1) 
+
+            TaskGoStraightToCoord(playerPed, 1732.1616, 2498.7124, 45.8185,1.0,8000,88.67,0)
+
+            Wait(2000)
+                       ProjectRP.Functions.Progressbar("clean_Shower", "Cleaning Showers", 45000, false, true, {
+                        disableMovement = true,
+                        disableCarMovement = true,
+                        disableMouse = false,
+                        disableCombat = true,
+                    }, {
+                        animDict = "amb@world_human_janitor@male@base",
+                        anim = "base",
+                        flags = 16,
+                    }, {}, {}, function() -- Done
+                        IsCleaning = false
+                        StopAnimTask(PlayerPedId(), "amb@world_human_janitor@male@base", "base", 1.0)
+                        JobDoneCleaning(1,4)
+                    end, function() -- Cancel
+                        IsCleaning = false
+                        StopAnimTask(PlayerPedId(), "amb@world_human_janitor@male@base", "base", 1.0)
+                        ProjectRP.Functions.Notify("Process Canceled..", "error")
+                    end)
+            else
+                ProjectRP.Functions.Notify("You are already cleaning the floor you mug..", "error")
+            end
+        else
+            ProjectRP.Functions.Notify("You are currently not serving any time?", "error")
+        end
+end)
