752c30262f32dd37eeace5bb92f140b512b0ba66
ProjectRP100 <projectrp100@gmail.com>
2022-04-12 17:36:04 -0700

    VU doorlocks

---
+++ resources/[prp]/prp-doorlock/configs/vanillaunicorn.lua
@@ -0,0 +1,65 @@
+
+
+-- main created by Parker
+Config.DoorList['main'] = {
+    objYaw = 29.999988555908,
+    audioRemote = false,
+    fixText = false,
+    authorizedJobs = { ['vu'] = 0 },
+    doorRate = 1.0,
+    objCoords = vec3(127.950066, -1298.506592, 29.419622),
+    distance = 2,
+    doorType = 'door',
+    objName = -1116041313,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- backroom created by Parker
+Config.DoorList['backroom'] = {
+    objYaw = 300.0,
+    audioRemote = false,
+    fixText = false,
+    authorizedJobs = { ['vu'] = 0 },
+    doorRate = 1.0,
+    objCoords = vec3(113.410095, -1296.260010, 29.435989),
+    distance = 2,
+    doorType = 'door',
+    objName = 390840000,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- office created by Parker
+Config.DoorList['office'] = {
+    objYaw = 29.999988555908,
+    audioRemote = false,
+    fixText = false,
+    authorizedJobs = { ['vu'] = 0 },
+    doorRate = 1.0,
+    objCoords = vec3(99.083069, -1293.689087, 29.440405),
+    distance = 2,
+    doorType = 'door',
+    objName = 390840000,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- officeback created by Parker
+Config.DoorList['officeback'] = {
+    objYaw = 210.0,
+    audioRemote = false,
+    fixText = false,
+    authorizedJobs = { ['vu'] = 0 },
+    doorRate = 1.0,
+    objCoords = vec3(96.091972, -1284.853760, 29.438784),
+    distance = 2,
+    doorType = 'door',
+    objName = 1695461688,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
