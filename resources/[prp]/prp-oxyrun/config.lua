51302ad86a2b3f76abd8cf1f1fc59751f2ea7091
Parker <aidanparker321@gmail.com>
2022-04-14 07:50:22 +0100

    oxy

--- resources/[prp]/prp-oxyrun/config.lua
+++ resources/[prp]/prp-oxyrun/config.lua
@@ -2,7 +2,7 @@ local ProjectRP = exports['prp-core']:GetCoreObject()
 Config = {}
 
 -- Oxy runs.
-Config.StartOxyPayment = 700 -- How much you pay at the start to start the run
+Config.StartOxyPayment = 500 -- How much you pay at the start to start the run
 
 Config.RunAmountMin = 8 -- Min drop offs the player does before it automatixally stops.
 Config.RunAmountMax = 12 -- Max drop offs the player does before it automatixally stops.
@@ -13,7 +13,7 @@ Config.PaymentMax = 580 -- Max money you get paid when RNG doesnt give you oxy, 
 Config.Item = "oxy" -- The item you receive from the oxy run. Should be oxy right??
 Config.OxyChance = 600 -- Percentage chance of getting oxy on the run. Multiplied by 100. 10% = 100, 20% = 200, 50% = 500, etc. Default 55%.
 Config.OxyAmountMin = 1 -- Min oxy you get when RN Jesus gives you oxy. Default: 1.
-Config.OxyAmountMax = 1 -- Max oxy you get when RN Jesus gives you oxy. Default: 1.
+Config.OxyAmountMax = 2 -- Max oxy you get when RN Jesus gives you oxy. Default: 1.
 
 Config.BigRewarditemChance = 200 -- Percentage of getting rare item on oxy run. Multiplied by 100. 0.1% = 1, 1% = 10, 20% = 200, 50% = 500, etc.
 Config.BigRewarditem = "trojan_usb" -- Rare item.
