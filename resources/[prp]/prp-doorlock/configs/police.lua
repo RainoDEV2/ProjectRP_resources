a763db4382fead814e6c75e02e80f3fa79f32422
ProjectRP100 <projectrp100@gmail.com>
2022-04-15 04:46:20 -0700

    Prison doorlocks

---
+++ resources/[prp]/prp-doorlock/configs/police.lua
@@ -0,0 +1,17 @@
+
+
+-- lockpick1 created by Parker
+Config.DoorList['lockpick1'] = {
+    objYaw = 0.0,
+    distance = 2,
+    audioRemote = false,
+    authorizedJobs = { ['police'] = 0 },
+    fixText = false,
+    objName = -1033001619,
+    doorRate = 1.0,
+    doorType = 'door',
+    objCoords = vec3(1827.299561, 2592.863037, 46.160942),
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
