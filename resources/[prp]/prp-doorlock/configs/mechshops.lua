35f370116213c153f068114fdbdf85aecdff12dc
ProjectRP100 <projectrp100@gmail.com>
2022-04-15 07:48:15 -0700

    Create mechshops.lua

---
+++ resources/[prp]/prp-doorlock/configs/mechshops.lua
@@ -0,0 +1,33 @@
+
+
+-- bennyspdm created by Parker
+Config.DoorList['bennyspdm'] = {
+    objYaw = 69.999923706055,
+    distance = 3,
+    audioRemote = false,
+    authorizedJobs = { ['realestate'] = 0 },
+    fixText = false,
+    objName = -427498890,
+    doorRate = 1.0,
+    doorType = 'garage',
+    objCoords = vec3(-44.188396, -1043.553833, 27.801605),
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- bennyssouth created by Parker
+Config.DoorList['bennyssouth'] = {
+    objYaw = 0.0,
+    distance = 4,
+    audioRemote = false,
+    authorizedJobs = { ['realestate'] = 0 },
+    fixText = false,
+    objName = -427498890,
+    doorRate = 1.0,
+    doorType = 'garage',
+    objCoords = vec3(-205.682831, -1310.682617, 30.295719),
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
