02ba3292fefec12d0fc8ea8e60804665af9a25bc
Parker <aidanparker321@gmail.com>
2022-04-16 21:19:11 +0100

    car pack

---
+++ resources/[Cars-Server]/pkcars/vehicle_names.lua
@@ -0,0 +1,10 @@
+Citizen.CreateThread(function()
+	
+	AddTextEntry('m5rb_vv', 'Unmarked BMW M5')
+	AddTextEntry('goldmk6', 'Golf Mk6')
+	AddTextEntry('um18charger', 'Unmarked Dodge Charger')
+	AddTextEntry('m3e36', 'BMW M3 E36')
+	AddTextEntry('speedoambo', 'Speedo Ambulance')
+	AddTextEntry('psp_bmwgs', 'BMW Bike')
+
+end)
