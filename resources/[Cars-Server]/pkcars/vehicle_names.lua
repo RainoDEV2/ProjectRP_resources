8b4df5078daf654853723eca7830ef3b19604990
Parker <aidanparker321@gmail.com>
2022-04-16 21:24:39 +0100

    name change

--- resources/[Cars-Server]/pkcars/vehicle_names.lua
+++ resources/[Cars-Server]/pkcars/vehicle_names.lua
@@ -5,6 +5,6 @@ Citizen.CreateThread(function()
 	AddTextEntry('um18charger', 'Unmarked Dodge Charger')
 	AddTextEntry('m3e36', 'BMW M3 E36')
 	AddTextEntry('speedoambo', 'Speedo Ambulance')
-	AddTextEntry('psp_bmwgs', 'BMW Bike')
+	AddTextEntry('psp_bmwgs', 'BMW PD Bike')
 
 end)
