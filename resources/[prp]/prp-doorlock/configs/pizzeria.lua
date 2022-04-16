4b2aec9b6b05297d7982faaac26463cbe224900e
ProjectRP100 <projectrp100@gmail.com>
2022-04-16 15:53:29 -0700

    Pizzeria doorlocks

---
+++ resources/[prp]/prp-doorlock/configs/pizzeria.lua
@@ -0,0 +1,65 @@
+
+
+-- enterance created by Parker
+Config.DoorList['enterance'] = {
+    doorRate = 1.0,
+    audioRemote = false,
+    distance = 2,
+    authorizedJobs = { ['pizza'] = 0 },
+    objYaw = 0.0,
+    fixText = false,
+    objCoords = vec3(286.744476, -964.398865, 29.600193),
+    objName = -950166942,
+    doorType = 'door',
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- staffonly created by Parker
+Config.DoorList['staffonly'] = {
+    doorRate = 1.0,
+    audioRemote = false,
+    distance = 1,
+    authorizedJobs = { ['pizza'] = 0 },
+    objYaw = 269.82647705078,
+    fixText = false,
+    objCoords = vec3(285.436646, -977.425659, 29.582153),
+    objName = 1289778077,
+    doorType = 'door',
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- backkitchen created by Parker
+Config.DoorList['backkitchen'] = {
+    doorRate = 1.0,
+    audioRemote = false,
+    distance = 1,
+    authorizedJobs = { ['pizza'] = 0 },
+    objYaw = 179.62640380859,
+    fixText = false,
+    objCoords = vec3(284.220032, -987.213074, 29.582153),
+    objName = 993120320,
+    doorType = 'door',
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
+
+-- backalley created by Parker
+Config.DoorList['backalley'] = {
+    doorRate = 1.0,
+    audioRemote = false,
+    distance = 2,
+    authorizedJobs = { ['pizza'] = 0 },
+    objYaw = 359.73797607422,
+    fixText = false,
+    objCoords = vec3(297.060822, -993.702881, 29.582153),
+    objName = 757543979,
+    doorType = 'door',
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
