c94218cfd3e70a942a1597d5b6b163453b434d3c
Parker <aidanparker321@gmail.com>
2022-04-17 18:11:11 +0100

    organisation

--- resources/[Cars-Server]/pkcars/vehicle_names.lua
+++ resources/[Cars-Server]/pkcars/vehicle_names.lua
@@ -1,10 +1,16 @@
 Citizen.CreateThread(function()
 	
-	AddTextEntry('m5rb_vv', 'Unmarked BMW M5')
-	AddTextEntry('goldmk6', 'Golf Mk6')
-	AddTextEntry('um18charger', 'Unmarked Dodge Charger')
-	AddTextEntry('m3e36', 'BMW M3 E36')
-	AddTextEntry('speedoambo', 'Speedo Ambulance')
+	-- Police/EMS
 	AddTextEntry('psp_bmwgs', 'BMW PD Bike')
+	AddTextEntry('speedoambo', 'Speedo Ambulance')
+	AddTextEntry('m5rb_vv', 'Unmarked BMW M5')
+	AddTextEntry('um18charger', 'Unmarked Dodge Charger')
+	
+	-- Job cars
+	AddTextEntry('goldmk6', 'Golf Mk6')
+	AddTextEntry('m3e36', 'BMW M3 E36')
 
+	--First Pack (4 Doors)
+	
+	
 end)
