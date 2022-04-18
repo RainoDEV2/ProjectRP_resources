2a8dc01f307c99f57d0d555f66a53cc86bf9f80f
Parker <aidanparker321@gmail.com>
2022-04-19 00:32:56 +0100

    door

--- resources/[prp]/prp-doorlock/configs/mechshops.lua
+++ resources/[prp]/prp-doorlock/configs/mechshops.lua
@@ -29,3 +29,20 @@ Config.DoorList['mechanic'] = {
     audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
     audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
 }
+
+-- lscustoms1 created by Parker
+Config.DoorList['lscustoms1'] = {
+    distance = 5,
+    locked = true,
+    objCoords = vec3(-356.090485, -134.771423, 40.012955),
+    audioRemote = false,
+    fixText = false,
+    objYaw = 249.99995422363,
+    doorRate = 1.0,
+    objName = -550347177,
+    doorType = 'garage',
+    authorizedJobs = { ['realestate'] = 0 },
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+}
