79857a31d6149d532a6e35014cc8916c0ce68ae8
Henry <henrynorth@outlook.com>
2022-04-21 23:49:08 +0100

    Steal shoes with hands up

--- resources/[prp]/prp-stealshoes/client/main.lua
+++ resources/[prp]/prp-stealshoes/client/main.lua
@@ -1,8 +1,13 @@
 local ProjectRP = exports['prp-core']:GetCoreObject()
 
-RegisterCommand("stealshoes", function()
-    TriggerEvent('prp-stealshoes:client:TheftShoe')
-end)
+local function IsTargetDead(playerId)
+    local retval = false
+    ProjectRP.Functions.TriggerCallback('police:server:isPlayerDead', function(result)
+        retval = result
+    end, playerId)
+    Wait(100)
+    return retval
+end
 
 RegisterNetEvent('prp-stealshoes:client:TheftShoe', function() -- This could be used in the radialmenu ;)
     local ped = PlayerPedId()
@@ -12,7 +17,7 @@ RegisterNetEvent('prp-stealshoes:client:TheftShoe', function() -- This could be 
             local playerId = GetPlayerServerId(player)
             if not IsPedInAnyVehicle(GetPlayerPed(player)) and not IsPedInAnyVehicle(ped) then
                 local oped = GetPlayerPed(player)
-                if (IsEntityPlayingAnim(oped, "dead", "dead_a", 3) or IsEntityPlayingAnim(oped, "amb@code_human_cower_stand@male@base", "base", 3) or IsEntityPlayingAnim(oped, "amb@code_human_cower@male@base", "base", 3) or  IsEntityPlayingAnim(oped, "random@arrests@busted", "idle_a", 3) or IsEntityPlayingAnim(oped, "mp_arresting", "idle", 3) or IsEntityPlayingAnim(oped, "random@mugging3", "handsup_standing_base", 3) or IsEntityPlayingAnim(oped, "missfbi5ig_22", "hands_up_anxious_scientist", 3) or IsEntityPlayingAnim(oped, "missfbi5ig_22", "hands_up_loop_scientist", 3) ) or IsTargetDead(playerId) then
+                if (IsEntityPlayingAnim(oped, "missminuteman_1ig_2", "handsup_base", 3) or IsEntityPlayingAnim(oped, "dead", "dead_a", 3) or IsEntityPlayingAnim(oped, "amb@code_human_cower_stand@male@base", "base", 3) or IsEntityPlayingAnim(oped, "amb@code_human_cower@male@base", "base", 3) or  IsEntityPlayingAnim(oped, "random@arrests@busted", "idle_a", 3) or IsEntityPlayingAnim(oped, "mp_arresting", "idle", 3) or IsEntityPlayingAnim(oped, "random@mugging3", "handsup_standing_base", 3) or IsEntityPlayingAnim(oped, "missfbi5ig_22", "hands_up_anxious_scientist", 3) or IsEntityPlayingAnim(oped, "missfbi5ig_22", "hands_up_loop_scientist", 3) ) or IsTargetDead(playerId) then
                     local hasShoes = GetPedDrawableVariation(oped, 6)
                     if hasShoes ~= 73 then
                         while not HasAnimDictLoaded("random@domestic") do
